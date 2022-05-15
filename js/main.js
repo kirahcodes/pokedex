cde44bffa1338fafa209dbde38b74cfb4f7dc926
Kirah Melancon <zamelancon@gmail.com>
2022-05-15 02:01:39 -0500

    Fixed abilities, types, pokemon ID and sprite.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,114 @@
-/* Design inspo:  https://codepen.io/joeykleinsorge/pen/oNWVQQe  */
-
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getPokemon);
+document.querySelector('button').addEventListener('click', fetchPokemon);
 let pokemonImage = document.querySelector('img');
 
-// TODO: Eventually add what the ability does. ex: https://pokeapi.co/api/v2/ability/50/
-function getAbilities(abilities) {
-	let abilitiesList = document.querySelector('.abilities'); // get the UL that will hold the abilities
+function fetchPokemon() {
+	const userInput = document.querySelector('input').value;
+	const fetchPokemonURL = `https://pokeapi.co/api/v2/pokemon/${userInput}`;
 
-	// get each ability in the object
-	abilities.forEach((el) => {
-		let abilityListItem = document.createElement('li');
-		let ability = el.ability['name']; // TODO: remove - if it exists in the ability name
-
-		abilityListItem.innerText = ability; // set the li to the ability
-		abilitiesList.append(abilityListItem); // append that ability to the list of abilities
-	});
-}
-
-function getPokemon() {
-	const pokemon = document.querySelector('input').value;
-	const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-
-	fetch(url)
+	/**
+	 * TODO: Fetch abilities
+	 */
+	fetch(fetchPokemonURL)
 		.then((res) => res.json()) // parse response as JSON
 		.then((data) => {
-			console.log(data);
-			getAbilities(data.abilities); // get abilites
+			buildPage(data);
+			// console.log(data);
+			// console.log(data.types); // get types
+			// getAbilities(data.abilities); // get abilites
 
-			pokemonImage.srcset =
-				data.sprites.other['official-artwork']['front_default']; // image
+			// pokemonImage.srcset =
+			// 	data.sprites.other['official-artwork']['front_default']; // image
 		})
 		.catch((err) => {
 			// if the pokemon searched doesn't exist in the DB
 			console.log(`Error: ${err}`);
 			console.log(`There was an error or the ${pokemon} does not exist.`);
+		});
+}
+
+function buildPage(data) {
+	let pokemonNameDisplay = document.querySelector('.pokemon-name');
+	let pokemonName = data.name;
+	let pokemonID = data.id;
+	let pokemonAbilities = data.abilities;
+	let pokemonTypes = data.types;
+	let pokemonSprite = data.sprites['front_default'];
+
+	// console.log(data);
+	// console.log(pokemonAbilities);
+
+	renderPokemonID(pokemonID);
+	renderPokemonTypes(pokemonTypes);
+	renderPokemonAbilities(pokemonAbilities);
+
+	pokemonNameDisplay.innerText = pokemonName;
+	document.querySelector('.pokemon-sprite').srcset = pokemonSprite;
+	// get necessary info
+	// add the text to the appropriate element
+	// append it to .card
+}
+
+// preference: function which changes the id to the format 001, 010, 100 like in the pokedex
+function renderPokemonID(pokemonID) {
+	let pokemonIdDisplay = document.querySelector('.pokemon-id');
+
+	if (pokemonID < 10) {
+		pokemonIdDisplay.innerText = `00${pokemonID}`;
+	} else if (pokemonID >= 10 && pokemonID < 100) {
+		pokemonIdDisplay.innerText = `0${pokemonID}`;
+	} else {
+		pokemonIdDisplay.innerText = pokemonID;
+	}
+}
+
+// renders the type found for the pokemon the user searched
+function renderPokemonTypes(pokemonTypes) {
+	let pokemonTypesDisplay = document.querySelector('.pokemon-types');
+
+	pokemonTypes.forEach((el) => {
+		let typeListItem = document.createElement('li');
+		let type = el.type['name'];
+
+		typeListItem.innerText = type;
+		pokemonTypesDisplay.appendChild(typeListItem);
+	});
+}
+
+// TODO: Eventually add what the ability does. ex: https://pokeapi.co/api/v2/ability/50/
+function renderPokemonAbilities(pokemonAbilities) {
+	let pokemonAbilitiesDisplay = document.querySelector('.pokemon-abilities');
+
+	// show all abilities available for pokemon
+	pokemonAbilities.forEach((el) => {
+		let abilityListItem = document.createElement('li');
+		let ability = el.ability['name'];
+
+		// get the description of the ability
+		let abilityDescription = fetchDescription(ability);
+
+		// set ability name and description text
+		abilityListItem.innerText =
+			'' + ability.replace('-', ' ') + ': ' + abilityDescription;
+
+		// append to DOM
+		pokemonAbilitiesDisplay.append(abilityListItem); // append that ability to the list of abilities
+	});
+}
+
+function fetchDescription(ability) {
+	const fetchAbilityURL = `https://pokeapi.co/api/v2/ability/${ability}`;
+
+	fetch(fetchAbilityURL)
+		.then((res) => res.json()) // parse response as JSON
+		.then((data) => {
+			// console.log(data);
+			let description = data['effect_entries'][1].effect;
+			console.log('description: ' + description);
+			return description;
+		})
+		.catch((err) => {
+			console.log(`Error: ${err}`);
+			console.log(`There was an error or the ${ability} does not exist.`);
 		});
 }
